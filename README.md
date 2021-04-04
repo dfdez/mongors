05d4cb9ef2836f5927a7b0a3a1e51d94a7405a94
Dfdez <danifer.inbox@gmail.com>
2021-04-04 17:20:36 +0200

    docs: Docs updated and some comments added

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
-## env
-- $HOST
-- $RS
-- $DB_HOST
-- $USER
-- $PASSWORD
-- $DB
-- $DBNAME
+## Documentation
+A simple docker-compose file to run a mongodb database in docker with replica set on.
+
+You can add a `.env` file to custom the replica set configuration:
+- `$RS` -> The replicas set name (Default: rs0)
+- `$HOST` -> The replica set host (Default: 127.0.0.1)
+- `$PORT` -> Teh replica set port (Default: 27017)
+
+Also you can `download` and `restore` any database by adding this variables:
+- `$DB_HOST` -> Host where download the database
+- `$USER` -> A username who has access to the database
+- `$PASSWORD` -> The password of the given $USER
+- `$DB` -> The name of the database that we want to clone
+- `$DBNAME` -> To change the name of the database to restore
--- mongo-config.sh
+++ mongo-config.sh
@@ -1,7 +1,9 @@
+# In case any of this variables does not exist set a default value
 [ -z $RS ] && RS=rs0
 [ -z $HOST ] && HOST=127.0.0.1
 [ -z $PORT ] && PORT=27017
 
+# Prepare the config script to active replica set
 cat << EOF > config.js
 config = {
   _id : "$RS",
@@ -16,6 +18,7 @@ if (rs_status) rs.reconfig(config, { force: true })
 else rs.initiate(config)
 EOF
 
+# Try to run the script in mongo and retry In case mongo still isn't running
 mongo --host $RS --port $PORT config.js
 while [ $? != 0 ]
 do
@@ -24,6 +27,7 @@ do
   mongo --host $RS --port $PORT config.js
 done
 
+# If $DB_HOST, $USER, $PASSWORD and $DB variables are setted, try to restore this database
 if [ ! -z $DB_HOST ] || [ ! -z $USER ] || [ ! -z $PASSWORD ] || [ ! -z $DB ]; then
   mongodump \
   --host $DB_HOST \
@@ -33,6 +37,7 @@ if [ ! -z $DB_HOST ] || [ ! -z $USER ] || [ ! -z $PASSWORD ] || [ ! -z $DB ]; th
   --authenticationDatabase admin \
   --db $DB
 
+  # If $DBNAME variable exist change the name of the database to restore
   [ ! -z $DBNAME ] && mv dump/$DB dump/$DBNAME
   mongorestore --host $RS dump
 fi
